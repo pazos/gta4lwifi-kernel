b5b2eb47d04d7df01af22fce3668bd749ed0d7d8
Martín Fdez <paziusss@gmail.com>
2021-12-22 01:36:31 +0100

    tweak build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
 export ARCH=arm64
-mkdir out
+[ -d out ] || mkdir out
+
+RELEASE=SM-T500-GSI-kernel.zip
+NAME=SM-T500-GSI
+FW_VERSION=T500XXU3BUJ1
 
 BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV="LOCALVERSION=-T500XXU3BUJ1"
+TC="ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/gta4lwifi_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+case $1 in
+	no-watchdog)
+		RELEASE=$NAME-no-watchdog.zip
+		DEFCONFIG=vendor/gta4lwifi_aosp_nowatchdog_defconfig
+		ID="-NO-WATCHDOG-$FW_VERSION"
+		;;
+	stock)
+		RELEASE=$NAME-stock.zip
+		DEFCONFIG=vendor/gta4lwifi_eur_open_defconfig
+		ID="-STOCK-$FW_VERSION"
+		;;
+	* )
+		RELEASE=$NAME.zip
+		DEFCONFIG=vendor/gta4lwifi_aosp_defconfig
+		ID="-$FW_VERSION"
+		;;
+esac
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+echo "Building out/$RELEASE"
+make -j8 -C $(pwd) O=$(pwd)/out LOCALVERSION=$ID $TC $DEFCONFIG
+make -j8 -C $(pwd) O=$(pwd)/out LOCALVERSION=$ID $TC
+rm -rf out/dist out/*.zip
+mkdir out/dist
+cp $(pwd)/out/arch/arm64/boot/Image $(pwd)/out/dist
+cp -pR $(pwd)/vendor/anykernel3/* $(pwd)/out/dist
+(cd out/dist && zip -r9 ../$RELEASE .)
